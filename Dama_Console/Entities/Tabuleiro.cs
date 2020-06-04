eb8c4f00752b0183e98907ac289b285912457c68
lincasaires <lucasairesgodinho1@gmail.com>
2020-06-03 21:22:32 -0300

    Tabuleiro Implementado

---
+++ Dama_Console/Entities/Tabuleiro.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dama_Console.Entities
+{
+    class Tabuleiro
+    {
+        public char[,] Casa { get; set; }
+        public int Linhas { get; set; }
+        public int Colunas { get; set; }
+
+        public Tabuleiro() { }
+
+        public Tabuleiro(int linhas,int colunas)
+        {
+            Linhas = linhas;
+            Colunas = colunas;
+
+            Casa = CriarTabuleiro();
+        }
+
+        public char[,] CriarTabuleiro()
+        {
+            char[,] mat = new char[Linhas, Colunas];
+            for (int i = 0; i < Linhas; i++)
+            {
+                for (int j = 0; j < Colunas; j++)
+                {
+                    mat[i, j] = '-';
+                }
+            }
+            return mat;
+        }
+
+        public void ImprimirTabuleiro()
+        {           
+            for (int i = 0; i < Linhas; i++)
+            {
+                Console.Write(i + " ");
+                for (int j = 0; j < Colunas; j++)
+                {
+                    Console.Write(Casa[i, j] + " "); ;
+                }
+                Console.WriteLine();
+            }
+            Console.Write("  ");
+            for (int i = 0; i < Linhas; i++)
+                Console.Write(i + " ");
+
+        }
+    }
+}
---
+++ Dama_Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using Dama_Console.Entities;
+using System;
+
+namespace Dama_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tabuleiro tab = new Tabuleiro(10,10);
+
+            tab.ImprimirTabuleiro();
+        }
+    }
+}
